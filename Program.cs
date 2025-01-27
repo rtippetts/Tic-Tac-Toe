84733d0a275fa02fb54f9ee94955c2d58baf8aef
RobertGardner13 <robbygardner26@gmail.com>
2025-01-27 08:41:27 -0700

    update
    comments

--- Program.cs
+++ Program.cs
@@ -1,1 +1,10 @@
-﻿
+﻿// Welcome the user to the game
+
+// Create a game board array to store the player's choices
+
+// Ask each player in turn for their choice and update the game board array
+
+//Print the board by calling the method in the supporting class
+
+//Check for a winner by calling the mehtod in the supporting class, and notify the players when a win has 
+// occured and which player won the game
