c1160bcd1517c49b57f54fe99fcfa843f017fc65
spencerlay10 <sklay10@gmail.com>
2025-01-27 08:45:45 -0700

    Player names

--- Program.cs
+++ Program.cs
@@ -1,10 +1,20 @@
-﻿// Welcome the user to the game
+﻿using Tic_Tac_Toe;
+
+// Welcome the user to the game
+
+
+// Ask for the players' names
+Console.WriteLine("Player 1, please enter your name: ");
+string player1 = Console.ReadLine();
+
+Console.WriteLine("Player 2, please enter your name: ");
+string player2 = Console.ReadLine();
 
 // Create a game board array to store the player's choices
 
 // Ask each player in turn for their choice and update the game board array
 
-//Print the board by calling the method in the supporting class
+// Print the board by calling the method in the supporting class
 
 //Check for a winner by calling the mehtod in the supporting class, and notify the players when a win has 
 // occured and which player won the game
