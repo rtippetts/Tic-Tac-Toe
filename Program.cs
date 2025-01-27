7bdac91135cf01d4c073700ddc62197d0ec59b33
RobertGardner13 <robbygardner26@gmail.com>
2025-01-27 08:47:21 -0700

    Created board array

--- Program.cs
+++ Program.cs
@@ -4,9 +4,11 @@ Console.WriteLine("Welcome to Tic Tac Toe!");
 
 // Create a game board array to store the player's choices
 
+char[] board = { '1', '2', '3', '4', '5', '6', '7', '8', '9' };
+
 // Ask each player in turn for their choice and update the game board array
 
 //Print the board by calling the method in the supporting class
 
-//Check for a winner by calling the mehtod in the supporting class, and notify the players when a win has 
+//Check for a winner by calling the method in the supporting class, and notify the players when a win has 
 // occured and which player won the game
