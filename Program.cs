889aa29d472277e1b8e57c527989f30cd40b32d4
RobertGardner13 <robbygardner26@gmail.com>
2025-01-27 09:17:44 -0700

    push

--- Program.cs
+++ Program.cs
@@ -10,14 +10,17 @@ string player1 = Console.ReadLine();
 Console.WriteLine("Player 2, please enter your name: ");
 string player2 = Console.ReadLine();
 
-
 // Create a game board array to store the player's choices
 
 char[] board = { '1', '2', '3', '4', '5', '6', '7', '8', '9' };
 
 // Ask each player in turn for their choice and update the game board array
 
+
+
 // Print the board by calling the method in the supporting class
+
+
 
 //Check for a winner by calling the method in the supporting class, and notify the players when a win has 
 // occured and which player won the game
